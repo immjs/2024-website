e1e63959feb7e8f34bd1e9ccb9a679850395aaa8
Juliet Wang <informathemusic@gmail.com>
2024-07-22 15:23:44 +0200

    Add vercel analytics

--- src/app/layout_contexts.tsx
+++ src/app/layout_contexts.tsx
@@ -4,6 +4,7 @@ import { tokenContext } from "@/components/csrf";
 import { ReactNode, useState } from "react";
 import { LightBtn } from "@/components/lightbtn";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export function ApplyContexts({ children, ogTheme, csrf }: ApplyContextsProps) {
   const [theme, setTheme] = useState<"light" | "dark">(ogTheme);
@@ -12,6 +13,7 @@ export function ApplyContexts({ children, ogTheme, csrf }: ApplyContextsProps) {
       className={`theme-setter ${theme === "light" ? "cat-latte bg-cat-surface0" : "bg-cat-crust"}`}
     >
       <SpeedInsights />
+      <Analytics />
       <LightBtn theme={theme} setTheme={setTheme} token={csrf} />
       <tokenContext.Provider value={csrf}>
         <themeContext.Provider value={theme}>{children}</themeContext.Provider>
