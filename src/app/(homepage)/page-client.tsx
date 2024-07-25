94019fd7f1dd6c59cc6a06b0fad6994f683c3b4b
Juliet Wang <informathemusic@gmail.com>
2024-07-25 02:40:55 +0200

    Add flags!

--- src/app/(homepage)/page-client.tsx
+++ src/app/(homepage)/page-client.tsx
@@ -6,10 +6,21 @@ import { MyBlogPosts } from "./myblogposts";
 import { MyPhotography } from "./myphotography";
 import { BitsOfPersonality, TetrioData } from "./bitsofpersonality";
 import { MySocials } from "./mysocials";
+import { useTheme } from "@/components/theme";
 
 export function Home({ status, tetrio }: { status: any; tetrio: TetrioData }) {
+  const theme = useTheme();
+
   return (
-    <div className="flex justify-center items-center px-6 py-[calc(50vh-125px-4.8rem)] pb-0 xl:pb-[320px] gutter-both-edges">
+    <div className="flex justify-center items-center px-6 py-[min(calc(50vh-125px-4.8rem),192px)] pb-0 xl:pb-[320px] gutter-both-edges">
+      <div className="absolute flex top-0 left-0 right-0 justify-center">
+        <img
+          className="absolute select-none"
+          src={`/homepage/doodles/${theme}/flags.svg`}
+          width={192}
+          height={192}
+        />
+      </div>
       <main className="flex flex-col gap-two text-cat-text justify-center max-w-[512px] xl:max-w-[800px]">
         <Introduction />
         <MyProjects />
