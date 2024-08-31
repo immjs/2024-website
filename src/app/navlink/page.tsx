0a20bc061084d533b4e89bf87a86db53540cbfde
Juliet Wang <informathemusic@gmail.com>
2024-08-31 22:08:53 +0200

    Fix NavLink ads stuck in iFrame

--- src/app/navlink/page.tsx
+++ src/app/navlink/page.tsx
@@ -20,7 +20,7 @@ export default async function Page() {
       <a href={randomLink[0]}>
         <img src={randomLink[1]} style={{ width: '100vw', height: '100vh', objectFit: 'cover' }} />
       </a>
-      <a href="https://dimden.dev/navlinkads/" style={{ border: '1px solid', bottom: 0, left: 0, fontSize: 10, padding: 2, fontFamily: 'serif', lineHeight: '1em', color: 'dimgray', position: 'absolute', backgroundColor: '#ffffffc0', borderRadius: "0 2px 0 0" }}>
+      <a href="https://dimden.dev/navlinkads/" target="_parent" style={{ border: '1px solid', bottom: 0, left: 0, fontSize: 10, padding: 2, fontFamily: 'serif', lineHeight: '1em', color: 'dimgray', position: 'absolute', backgroundColor: '#ffffffc0', borderRadius: "0 2px 0 0" }}>
         NavLink Ads
       </a>
     </body>
