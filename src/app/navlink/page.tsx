2ca1d99be89a111cb931abc73be20e7fa3be9ad9
Juliet Wang <informathemusic@gmail.com>
2024-09-01 00:42:46 +0200

    Fix NavLink Ads (yet agaibn)

--- src/app/navlink/page.tsx
+++ src/app/navlink/page.tsx
@@ -17,7 +17,7 @@ export default async function Page() {
   return (
     <body style={{ overflow: 'hidden' }}>
       <meta httpEquiv="refresh" content="30" />
-      <a href={randomLink[0]}>
+      <a href={randomLink[0]} target="_parent">
         <img src={randomLink[1]} style={{ width: '100vw', height: '100vh', objectFit: 'cover' }} />
       </a>
       <a href="https://dimden.dev/navlinkads/" target="_parent" style={{ border: '1px solid', bottom: 0, left: 0, fontSize: 10, padding: 2, fontFamily: 'serif', lineHeight: '1em', color: 'dimgray', position: 'absolute', backgroundColor: '#ffffffc0', borderRadius: "0 2px 0 0" }}>
