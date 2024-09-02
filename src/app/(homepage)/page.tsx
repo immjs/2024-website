b8601038fbe817d09f8180ebce5650087d168c98
Juliet Wang <informathemusic@gmail.com>
2024-09-02 09:35:32 +0200

    Prevent site timeout if third party down

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -6,54 +6,72 @@ import { sql } from "@vercel/postgres";
 import { tetrioFetch } from "@/lib/tetrio-fetch";
 import json5 from "json5";
 import { headers } from "next/headers";
+import { TetrioData } from "./bitsofpersonality";
 
 export default async function Page() {
   const [status, [data40l, dataTetra], [prev, host, next]] = await Promise.all([
     sql`SELECT * FROM Status LIMIT 1;`.then((v) => v.rows[0]),
+    Promise.race([
+      (async (): Promise<TetrioData> => {
+        try {
+          const {
+            record: record40l,
+          } = await tetrioFetch(`/users/${process.env.TETRIO_USERNAME}/summaries/40l`);
+          const recordleague = await tetrioFetch(`/users/${process.env.TETRIO_USERNAME}/summaries/league`);
+          const user = await tetrioFetch(
+            `/users/${process.env.TETRIO_USERNAME}`,
+          );
+          const { news } = await tetrioFetch(`/news/user_${user._id}`);
+          return [
+            {
+              time: record40l.results.stats.finaltime as number,
+              piecesPlaced: record40l.results.stats.piecesplaced as number,
+              at: new Date(record40l.ts).getTime(),
+            },
+            {
+              rank: recordleague.rank as string,
+              rating: recordleague.tr as number,
+              at: new Date(
+                news.find((v: any) => v.type === "rankup" && v.data.rank)?.ts || 0,
+              ).getTime(),
+            },
+          ];
+        } catch (err) {
+          console.log(err);
+          return [
+            {
+              time: -1,
+              piecesPlaced: -1,
+              at: 0,
+            },
+            {
+              rank: "z",
+              rating: -1,
+              at: 0,
+            },
+          ];
+        }
+      })(),
+      new Promise<TetrioData>((r) => setTimeout(() => r([
+        {
+          time: -1,
+          piecesPlaced: -1,
+          at: 0,
+        },
+        {
+          rank: "z",
+          rating: -1,
+          at: 0,
+        },
+      ]), 500)),
+    ]),
     (async () => {
-      try {
-        const {
-          record: record40l,
-        } = await tetrioFetch(`/users/${process.env.TETRIO_USERNAME}/summaries/40l`);
-        const recordleague = await tetrioFetch(`/users/${process.env.TETRIO_USERNAME}/summaries/league`);
-        const user = await tetrioFetch(
-          `/users/${process.env.TETRIO_USERNAME}`,
-        );
-        const { news } = await tetrioFetch(`/news/user_${user._id}`);
-        return [
-          {
-            time: record40l.results.stats.finaltime as number,
-            piecesPlaced: record40l.results.stats.piecesplaced as number,
-            at: new Date(record40l.ts).getTime(),
-          },
-          {
-            rank: recordleague.rank as string,
-            rating: recordleague.tr as number,
-            at: new Date(
-              news.find((v: any) => v.type === "rankup" && v.data.rank)?.ts || 0,
-            ).getTime(),
-          },
-        ];
-      } catch (err) {
-        console.log(err);
-        return [
-          {
-            time: -1,
-            piecesPlaced: -1,
-            at: 0,
-          },
-          {
-            rank: "z",
-            rating: -1,
-            at: 0,
-          },
-        ];
-      }
-    })(),
-    (async () => {
-      const links = await fetch(`https://palette.nekoweb.org/sites.js?t=${Date.now()}`)
+      const links = await Promise.race([
+        fetch(`https://palette.nekoweb.org/sites.js?t=${Date.now()}`)
           .then((v) => v.text())
-          .then((v) => v.match(/default ([^;]+);/i)?.[1]);
+          .then((v) => v.match(/default ([^;]+);/i)?.[1]),
+        new Promise<undefined>((r) => setTimeout(() => r(undefined), 500)),
+      ]);
 
       const host = headers().get('host')!;
       if (!links) return [host, host, host];
